43dbf34f3057e1cc00c287a1aee93a56c4bc39bd
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2026-03-13 20:26:52 +0530

    Adding ESLint configuration to enforce JavaScript code quality and consistency

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs'
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single']
+    }
+  }
+];
